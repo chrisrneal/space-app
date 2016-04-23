1ad1542620ee14aec9e2158c471b4420027a2cd6
Paul Nogas <paulnogas@gmail.com>
2016-04-23 19:14:48 -0400

    for chris to look at

--- space/views.py
+++ space/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'space/homepage.html', {})
+    symptoms = Symptom.objects
+    return render(request, 'space/homepage.html', {'pins' : symptoms})
     
 def about(request):
     return render(request, 'space/about.html', {})
